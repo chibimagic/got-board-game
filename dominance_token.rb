adae1341ca06b6156235f6b7897ef9c2dc56a8da
chibimagic <git@chibimagic.org>
2014-06-21 21:04:35 -0700

    Initial commit: dominance tokens

---
+++ dominance_token.rb
@@ -0,0 +1,31 @@
+class DominanceToken
+end
+
+class UsableDominanceToken < DominanceToken
+  attr_reader :used
+
+  def initialize
+    @used = false
+  end
+
+  def use
+    if @used
+      raise 'This token has already been used'
+    end
+
+    @used = true
+  end
+
+  def reset
+    @used = false
+  end
+end
+
+class IronThroneToken < DominanceToken
+end
+
+class ValyrianSteelBladeToken < UsableDominanceToken
+end
+
+class MessengerRavenToken < UsableDominanceToken
+end
---
+++ test_dominance_token.rb
@@ -0,0 +1,27 @@
+require 'test/unit'
+require_relative 'dominance_token.rb'
+
+class TestGame < Test::Unit::TestCase
+  def test_usable_token
+    token_classes = [ValyrianSteelBladeToken, MessengerRavenToken]
+    token_classes.each do |token_class|
+      t = token_class.new
+      assert_equal(false, t.used)
+      t.use
+      assert_equal(true, t.used)
+      t.reset
+      assert_equal(false, t.used)
+    end
+  end
+
+  def test_usable_token_reuse
+    token_classes = [ValyrianSteelBladeToken, MessengerRavenToken]
+    token_classes.each do |token_class|
+      assert_raise(RuntimeError) {
+        t = token_class.new
+        t.use
+        t.use
+      }
+    end
+  end
+end
