3909db0a4a71804ce4df3b77e049f5a11659372a
chibimagic <git@chibimagic.org>
2014-06-27 18:48:10 -0700

    Automatically include all files in test directory when testing

--- test/test.rb
+++ test/test.rb
@@ -4,17 +4,5 @@ SimpleCov.start
 require 'test/unit'
 require_relative '../lib/game.rb'
 
-require_relative 'test_area.rb'
-require_relative 'test_dominance_token.rb'
-require_relative 'test_game.rb'
-require_relative 'test_house_card.rb'
-require_relative 'test_house.rb'
-require_relative 'test_influence_track.rb'
-require_relative 'test_map.rb'
-require_relative 'test_neutral_force_tokens.rb'
-require_relative 'test_order_token.rb'
-require_relative 'test_power_pool.rb'
-require_relative 'test_westeros_card.rb'
-require_relative 'test_westeros_deck.rb'
-require_relative 'test_wildling_card.rb'
-require_relative 'test_wildling_track.rb'
+Dir.chdir(File.dirname(__FILE__))
+Dir.glob('test_*').each { |file| require_relative file }
