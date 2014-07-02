95c3cffdcba12c0f8ff3f30eeae310535d9fb952
chibimagic <git@chibimagic.org>
2014-07-02 11:18:14 -0700

    Handle non-JSON input

--- routes.rb
+++ routes.rb
@@ -3,19 +3,21 @@ require 'JSON'
 require_relative 'lib/game.rb'
 
 post '/games' do
-  request.body.rewind
-  data = JSON.parse(request.body.read)
-  house_map = {
-    'Stark' => HouseStark,
-    'Lannister' => HouseLannister,
-    'Baratheon' => HouseBaratheon,
-    'Greyjoy' => HouseGreyjoy,
-    'Tyrell' => HouseTyrell,
-    'Martell' => HouseMartell,
-  }
   begin
+    request.body.rewind
+    data = JSON.parse(request.body.read)
+    house_map = {
+      'Stark' => HouseStark,
+      'Lannister' => HouseLannister,
+      'Baratheon' => HouseBaratheon,
+      'Greyjoy' => HouseGreyjoy,
+      'Tyrell' => HouseTyrell,
+      'Martell' => HouseMartell,
+    }
     houses = data.map { |house, player_name| house_map[house].new(player_name) }
     g = Game.new(houses)
+  rescue JSON::ParserError => e
+    'JSON input expected'
   rescue RuntimeError => e
     e.message
   end
