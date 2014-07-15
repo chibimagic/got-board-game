00eb50bb9e7d5a2c4eec8481e676dcbe275c19f0
chibimagic <git@chibimagic.org>
2014-07-15 15:22:07 -0700

    Stop using class methods hard wired to @browser

--- test/test_routes.rb
+++ test/test_routes.rb
@@ -1,47 +1,41 @@
 class TestRoutes < MiniTest::Test
-  def get(relative_url)
-    @browser.get(relative_url).body
-  end
-
-  def post(relative_url, body)
-    @browser.post(relative_url, body.to_json).body
-  end
-
   def setup
     @browser = Rack::Test::Session.new(Rack::MockSession.new(Sinatra::Application))
     usernames = ['a', 'b', 'c', 'd', 'e', 'f']
     usernames.each do |username|
-      post('/users', { 'username' => username, 'password' => 'password', 'player_name' => username })
+      @browser.post('/users', { 'username' => username, 'password' => 'password', 'player_name' => username }.to_json)
     end
-    post('/session', { 'username' => usernames[0], 'password' => 'password' })
+    @browser.post('/session', { 'username' => usernames[0], 'password' => 'password' }.to_json)
   end
 
   def test_create_user_invalid
-    response = post('/users', '')
-    assert_equal('JSON input expected', response)
-    response = post('/users', { 'username' => 'username' })
-    assert_equal('Format: {"username":"jdoe","password":"password","player_name":"John"}', response)
-    response = post('/users', { 'password' => 'password' })
-    assert_equal('Format: {"username":"jdoe","password":"password","player_name":"John"}', response)
-    response = post('/users', { 'player_name' => 'player name' })
-    assert_equal('Format: {"username":"jdoe","password":"password","player_name":"John"}', response)
+    response = @browser.post('/users', '')
+    assert_equal('JSON input expected', response.body)
+    response = @browser.post('/users', { 'username' => 'username' }.to_json)
+    assert_equal('Format: {"username":"jdoe","password":"password","player_name":"John"}', response.body)
+    response = @browser.post('/users', { 'password' => 'password' }.to_json)
+    assert_equal('Format: {"username":"jdoe","password":"password","player_name":"John"}', response.body)
+    response = @browser.post('/users', { 'player_name' => 'player name' }.to_json)
+    assert_equal('Format: {"username":"jdoe","password":"password","player_name":"John"}', response.body)
   end
 
   def test_create_game_invalid
-    response = post('/games', '')
-    assert_equal('JSON input expected', response)
-    response = post('/games', {})
-    assert_equal('Cannot create a game that does not include yourself: a, {}', response)
-    response = post('/games', { 'HouseStark' => 'a' })
-    assert_equal('A Game of Thrones (second edition) can only be played with 3-6 players, not 1', response)
-    response = post('/games', { 'HouseStark' => 'a', 'HouseLannister' => 'b' })
-    assert_equal('A Game of Thrones (second edition) can only be played with 3-6 players, not 2', response)
+    response = @browser.post('/games', '')
+    assert_equal('JSON input expected', response.body)
+    response = @browser.post('/games', {}.to_json)
+    assert_equal('Cannot create a game that does not include yourself: a, {}', response.body)
+    response = @browser.post('/games', { 'HouseStark' => 'b' }.to_json)
+    assert_equal('Cannot create a game that does not include yourself: a, {"HouseStark"=>"b"}', response.body)
+    response = @browser.post('/games', { 'HouseStark' => 'a' }.to_json)
+    assert_equal('A Game of Thrones (second edition) can only be played with 3-6 players, not 1', response.body)
+    response = @browser.post('/games', { 'HouseStark' => 'a', 'HouseLannister' => 'b' }.to_json)
+    assert_equal('A Game of Thrones (second edition) can only be played with 3-6 players, not 2', response.body)
   end
 
   def test_create_retrieve
-    response = post('/games', { 'HouseStark' => 'a', 'HouseLannister' => 'b', 'HouseBaratheon' => 'c' })
-    game_id = JSON.parse(response)['game_id']
-    response = get('/games/' + game_id.to_s)
-    assert_kind_of(String, response)
+    response = @browser.post('/games', { 'HouseStark' => 'a', 'HouseLannister' => 'b', 'HouseBaratheon' => 'c' }.to_json)
+    game_id = JSON.parse(response.body)['game_id']
+    response = @browser.get('/games/' + game_id.to_s)
+    assert_kind_of(String, response.body)
   end
 end
