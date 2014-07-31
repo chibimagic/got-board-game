c54516cb77303d80ed0749690b25bef6f3253e6d
chibimagic <git@chibimagic.org>
2014-07-30 19:41:24 -0700

    Nice failure message for refute_raises

--- test/test.rb
+++ test/test.rb
@@ -7,8 +7,20 @@ require_relative '../routes.rb'
 require 'set'
 
 class MiniTest::Test
-  def refute_raises(*)
-    yield
+  def refute_raises(msg = nil)
+    if msg.nil?
+      msg = ''
+    else
+      msg += "\n"
+    end
+
+    begin
+      yield
+    rescue => e
+      assert(false, proc {
+        exception_details(e, msg + 'Exception not expected, but raised anyway')
+      })
+    end
   end
 end
 
