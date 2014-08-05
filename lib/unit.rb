ae57906ee25a033c2c4db154af2bb8b985d96a56
chibimagic <git@chibimagic.org>
2014-08-05 16:50:20 -0700

    Unit combat strength

--- lib/unit.rb
+++ lib/unit.rb
@@ -2,6 +2,7 @@ class Unit < HouseToken
   attr_reader :routed
 
   MUSTERING_COST = 0
+  COMBAT_STRENGTH = 0
 
   def initialize(house_class, routed)
     raise 'Invalid routed' unless [true, false].include?(routed)
@@ -33,6 +34,10 @@ class Unit < HouseToken
       @routed == o.routed
   end
 
+  def combat_strength(attacked_area_class)
+    self.class::COMBAT_STRENGTH
+  end
+
   def route
     @routed = true
   end
@@ -45,19 +50,32 @@ end
 class Footman < Unit
   TITLE = 'Footman'
   MUSTERING_COST = 1
+  COMBAT_STRENGTH = 1
 end
 
 class Knight < Unit
   TITLE = 'Knight'
   MUSTERING_COST = 2
+  COMBAT_STRENGTH = 2
 end
 
 class Ship < Unit
   TITLE = 'Ship'
   MUSTERING_COST = 1
+  COMBAT_STRENGTH = 1
 end
 
 class SiegeEngine < Unit
   TITLE = 'Siege Engine'
   MUSTERING_COST = 2
+
+  def combat_strength(attacked_area_class)
+    if !attacked_area_class.nil? &&
+      attacked_area_class < LandArea &&
+      (attacked_area_class::STRONGHOLD || attacked_area_class::CASTLE)
+      4
+    else
+      0
+    end
+  end
 end
--- test/test_unit.rb
+++ test/test_unit.rb
@@ -5,4 +5,22 @@ class TestUnit < MiniTest::Test
     refute_equal(Footman.create_new(HouseStark), Knight.create_new(HouseStark))
     refute_equal(Unit.create_new(HouseStark), Footman.create_new(HouseStark))
   end
+
+  def test_combat_strength
+    assert_equal(1, Footman.create_new(HouseStark).combat_strength(nil))
+    assert_equal(1, Footman.create_new(HouseStark).combat_strength(CastleBlack))
+    assert_equal(1, Footman.create_new(HouseStark).combat_strength(Winterfell))
+
+    assert_equal(2, Knight.create_new(HouseStark).combat_strength(nil))
+    assert_equal(2, Knight.create_new(HouseStark).combat_strength(CastleBlack))
+    assert_equal(2, Knight.create_new(HouseStark).combat_strength(Winterfell))
+
+    assert_equal(1, Ship.create_new(HouseStark).combat_strength(nil))
+    assert_equal(1, Ship.create_new(HouseStark).combat_strength(CastleBlack))
+    assert_equal(1, Ship.create_new(HouseStark).combat_strength(Winterfell))
+
+    assert_equal(0, SiegeEngine.create_new(HouseStark).combat_strength(nil))
+    assert_equal(0, SiegeEngine.create_new(HouseStark).combat_strength(CastleBlack))
+    assert_equal(4, SiegeEngine.create_new(HouseStark).combat_strength(Winterfell))
+  end
 end
