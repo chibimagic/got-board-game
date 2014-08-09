a358f1f56c044a8e9260642705579ebe0a6ee4e5
chibimagic <git@chibimagic.org>
2014-08-08 19:02:22 -0700

    House cards in combat

--- lib/combat.rb
+++ lib/combat.rb
@@ -2,27 +2,40 @@ class Combat
   attr_reader \
     :attacking_area,
     :defending_area,
-    :attacking_units
+    :attacking_units,
+    :attacking_house_card,
+    :defending_house_card
 
   def initialize(
     attacking_area,
     defending_area,
-    attacking_units
+    attacking_units,
+    attacking_house_card,
+    defending_house_card
   )
     raise 'Invalid attacking area' unless attacking_area.is_a?(Area)
     raise 'Invalid defending area' unless defending_area.is_a?(Area)
     raise 'Invalid attacking units' unless attacking_units.is_a?(Array) && attacking_units.all? { |unit| unit.is_a?(Unit) }
+    raise 'Invalid attacking house card' unless attacking_house_card.is_a?(HouseCard)
+    raise 'Invalid defending house card' unless defending_house_card.is_a?(HouseCard)
 
     @attacking_area = attacking_area
     @defending_area = defending_area
     @attacking_units = attacking_units
+    @attacking_house_card = attacking_house_card
+    @defending_house_card = defending_house_card
   end
 
   def self.create_new(attacking_area, defending_area, attacking_units)
+    attacking_house_card = nil
+    defending_house_card = nil
+
     new(
       attacking_area,
       defending_area,
-      attacking_units
+      attacking_units,
+      attacking_house_card,
+      defending_house_card
     )
   end
 
@@ -32,7 +45,15 @@ class Combat
     attacking_area = data['attacking_area']
     defending_area = data['defending_area']
     attacking_units = data['attacking_units']
+    attacking_house_card = data['attacking_house_card']
+    defending_house_card = data['defending_house_card']
 
-    new(attacking_area, defending_area, attacking_units)
+    new(
+      attacking_area,
+      defending_area,
+      attacking_units,
+      attacking_house_card,
+      defending_house_card
+    )
   end
 end
