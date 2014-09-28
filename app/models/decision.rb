2ddeb3cdab7c55bf7323fb746f0e5851aa7ef5a6
chibimagic <git@chibimagic.org>
2014-09-28 10:45:35 -0700

    Decision: Add types

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,11 +1,18 @@
 class Decision
-  attr_reader :house_class, :options
+  attr_reader :house_class, :options, :type
 
-  def initialize(house_class, options)
+  TYPES = [
+    :highest_bidder,
+    :lowest_bidder
+  ]
+
+  def initialize(house_class, options, type)
     raise 'Invalid house class' unless house_class.is_a?(Class) && attacking_house_class < House
     raise 'Invalid options' unless options.is_a?(Array) && options.length > 0
+    raise 'Invalid type' unless TYPES.include?(type)
 
     @house_class = house_class
     @options = options
+    @type = type
   end
 end
